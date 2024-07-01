eb3361a62ceb168743ac8fc97d30bac4aa3a2b4a
RBuck8339 <ronanwb2015@outlook.com>
2024-06-30 22:33:50 -0400

    Made a simple function to call data from the Alpha Vantage API and convert it into a dataframe

---
+++ helper.py
@@ -0,0 +1,19 @@
+# Currently just imports the data and tests with it
+
+import requests
+import pandas as pd 
+from io import StringIO
+
+
+my_key = 'DVC3S0LMT7YNYDC3'
+  
+def get_data(symbol: str):
+   url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&outputsize=full&datatype=csv&apikey={my_key}'
+   
+   data = requests.get(url)
+   data = StringIO(data.text)
+   data = pd.read_csv(data, index_col = 'timestamp')
+
+   print(data.head())
+   
+get_data('MSFT')
