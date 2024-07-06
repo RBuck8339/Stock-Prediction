adce483ca28df34f11f8c01005d2e0eb3a052377
RBuck8339 <ronanwb2015@outlook.com>
2024-07-05 21:47:51 -0400

    Accidentally removed code

---
+++ LSTM.py
@@ -0,0 +1,15 @@
+import torch.nn as nn
+
+class LSTM(nn.Module):
+    def __init__(self):
+        pass
+    
+    
+    def forward(self):
+        pass
+    
+    
+    def loss(self):
+        pass
+    
+    
