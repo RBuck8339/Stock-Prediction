e7425cd0ad8a1f9075dca831d52125e4c051fdb1
RBuck8339 <ronanwb2015@outlook.com>
2024-07-07 22:46:47 -0400

    Added the init method for the LSTM model to set up layers. Inspired by models I saw online.

--- LSTM.py
+++ LSTM.py
@@ -1,11 +1,16 @@
 import torch.nn as nn
 
+
 class LSTM(nn.Module):
-    def __init__(self):
-        pass
+    def __init__(self, input_size, hidden_size, num_layers, dropout):
+        self.linear_1 = nn.Linear(in_features=input_size, hidden_size=hidden_size)
+        self.activation = nn.ReLU()
+        self.lstm_layers = nn.LSTM(input_size=hidden_size, hidden_size=hidden_size, num_layers=num_layers)
+        self.dropout_layer = nn.Dropout(p=dropout)
+        self.linear_2 = nn.Linear(num_layers * hidden_size, out_features=1)
     
     
-    def forward(self):
+    def forward(self, x):
         pass
     
     
